ee35e73dd591ce770c015660916dc8640c336af2
Ivans <ivnsimakhin@gmail.com>
2020-07-09 14:17:28 +0300

    Started hash

---
+++ src/common/hash/hash.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ivan on 09.07.20.
+//
+
+#include "hash.h"
---
+++ src/common/hash/hash.h
@@ -0,0 +1,14 @@
+//
+// Created by ivan on 09.07.20.
+//
+
+#ifndef INFO_SEARCH_HASH_H
+#define INFO_SEARCH_HASH_H
+
+#include <cstdint>
+#include <>
+#include <string.h>
+
+uint64_t hash(const wchar_t* str);
+
+#endif //INFO_SEARCH_HASH_H
